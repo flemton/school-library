436d4f4bd3844f71ee20e62686c73fa02018b70e
Osman Alhassan <oalhassan847@gmail.com>
2023-06-30 03:21:18 +0000

    Add Student-Classroom relationships

--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,14 @@
 class Classroom
-  attr_accessor :@label
+  attr_accessor :label
+  attr_reader :students
+
   def initialize(label)
-      @label - label
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student)
+    student.classroom = self
   end
 end
--- student.rb
+++ student.rb
@@ -1,8 +1,14 @@
 require_relative 'person'
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+  end
+
+  def to_class
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
